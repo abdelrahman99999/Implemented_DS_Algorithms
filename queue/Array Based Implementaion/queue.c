10d5d72417e78e64ee510adabba535345e31504f
Abdelrahman Elsayed Mohamed <60861577+abdelrahman99999@users.noreply.github.com>
2021-07-19 02:47:06 +0200

    Add files via upload

---
+++ queue/Array Based Implementaion/queue.c
@@ -0,0 +1,117 @@
+#include<stdio.h>
+typedef enum { false, true }boolean;
+
+//you need to set queue size and it's elements type
+#define QueueEntry int
+#define MaxQueue 100
+typedef struct queue {
+	int front;
+	int rear;
+	int size;
+	QueueEntry entry[MaxQueue];
+}Queue;
+
+void initQueue(Queue *pq) {
+	pq->front = 0;
+	pq->rear = -1; //so when add element we increase rear so when there is one element rear=front
+	pq->size = 0;
+}
+
+void append(QueueEntry e, Queue *pq) {
+	/*
+	if(pq->rear==(MaxQueue-1))pq->rear=0;
+	else pq->rear++;
+	*/
+	//or using this ,will do the same
+	pq->rear= (pq->rear+1)% MaxQueue;
+	pq->entry[pq->rear] = e;
+	pq->size ++;
+}
+
+boolean queueFull(Queue *pq) {
+	return pq->size == MaxQueue;
+}
+
+void serve(QueueEntry *pe, Queue *pq) {
+	//get first element and remove it (user think)
+	*pe = pq->entry[pq->front];
+	pq->front = (pq->front + 1) % MaxQueue;
+	/*
+	if(pq->front== MaxQueue-1)pq->front=0;
+	else pq->front++;
+	*/
+	pq->size--;
+}
+void queuefirst(QueueEntry *pe, Queue *pq) {
+	//to get first element without remove it (user think)
+	*pe = pq->entry[pq->front];
+}
+
+boolean queueEmpty(Queue *pq) {
+	return !pq->size;
+}
+
+int queueSize(Queue *pq) {
+	return pq->size ;
+}
+
+void clearQueue(Queue *pq) {
+	//same as stack,the implementation here is like implementaion of initQueue but the user doesn't know this
+	//as we know in linked based stack ,implementaion of clear not like as implementaion of init,so we will here make 2 functions
+	// to make interface of array based like interface of linked based
+	pq->front = 0;
+	pq->rear = -1;
+	pq->size = 0;
+}
+
+//to access every element and make a function to deal with it,this function can change queue values as i pass ( &pq->entry[pos] )
+void traverseQueue(Queue *pq, void(*pf)(QueueEntry *e)) {
+	int pos = pq->front;
+	for (int s = 0; s < pq->size; s++) {
+		(*pf)(&pq->entry[pos]);
+		pos = (pos + 1) % MaxQueue;
+	}
+}
+//to access every element and make a function to deal with it,this function can not change queue values as i pass ( pq->entry[pos] )
+void traverseQueue2(Queue *pq, void(*pf)(QueueEntry e)) {
+	int pos = pq->front;
+	for (int s = 0; s < pq->size; s++) {
+		(*pf)(pq->entry[pos]);
+		pos = (pos + 1) % MaxQueue;
+	}
+}
+
+void add2(QueueEntry *e) {
+	*e += 2;
+}
+
+void display(QueueEntry e) {
+	printf("%d ", e);
+}
+
+int main() {
+	//test
+	Queue q1;
+	initQueue(&q1);
+	printf("is queue empty? %d\n", queueEmpty(&q1));
+	printf("the size of queue: %d\n", queueSize(&q1));
+	if (!queueFull(&q1))append(5, &q1);
+	if (!queueFull(&q1))append(10, &q1);
+	if (!queueFull(&q1))append(15, &q1);
+	if (!queueFull(&q1))append(20, &q1);
+	if (!queueFull(&q1))append(25, &q1);
+	printf("is queue empty? %d\n", queueEmpty(&q1));
+	printf("the size of stack: %d\n", queueSize(&q1));
+	traverseQueue2(&q1, &display);
+	printf("\n");
+	int x = 3;
+	if (!queueEmpty(&q1))serve(&x, &q1);
+	printf("x value after changing: %d\n", x);
+	printf("the size of queue: %d\n", queueSize(&q1));
+	queuefirst(&x, &q1);
+	printf("x value after changing: %d\n", x);
+	traverseQueue(&q1, &add2);
+	traverseQueue2(&q1, &display);
+	printf("\n");
+	return 0;
+}
---
+++ queue/Array Based Implementaion/queue.cpp
@@ -0,0 +1,123 @@
+#include<iostream>
+using namespace std;
+#define MaxQueue 100
+
+template<class T>
+class Queue {
+private:
+	int front;
+	int rear;
+	int size;
+	T entry[MaxQueue];
+public:
+	Queue() {
+		front = 0;
+		rear = -1; //so when add element we increase rear so when there is one element rear=front
+		size = 0;
+	}
+
+	void append(T e) {
+		/*
+		if(rear==(MaxQueue-1))rear=0;
+		else rear++;
+		*/
+		//or using this ,will do the same
+		rear = (rear + 1) % MaxQueue;
+		entry[rear] = e;
+		size++;
+	}
+
+	bool queueFull() {
+		return size == MaxQueue;
+	}
+
+
+	void serve(T *pe) {
+		//get first element and remove it (user think)
+		*pe = entry[front];
+		front = (front + 1) % MaxQueue;
+		/*
+		if(front== MaxQueue-1)front=0;
+		else front++;
+		*/
+		size--;
+	}
+	void queuefirst(T *pe) {
+		//to get first element without remove it (user think)
+		*pe =entry[front];
+	}
+
+
+	bool queueEmpty() {
+		return !size;
+	}
+
+	int queueSize() {
+		return size;
+	}
+
+	void clearQueue() {
+		//same as stack,the implementation here is like implementaion of initQueue(constructor) but the user doesn't know this
+		//as we know in linked based stack ,implementaion of clear not like as implementaion of init(constructor),so we will here make 2 functions
+		// to make interface of array based like interface of linked based
+		front = 0;
+		rear = -1;
+		size = 0;
+	}
+
+	//to access every element and make a function to deal with it,this function can change queue values as i pass ( &pq->entry[pos] )
+	void traverseQueue(void(*pf)(T *e)) {
+		int pos = front;
+		for (int s = 0; s < size; s++) {
+			(*pf)(&entry[pos]);
+			pos = (pos + 1) % MaxQueue;
+		}
+	}
+	//to access every element and make a function to deal with it,this function can not change queue values as i pass ( pq->entry[pos] )
+	void traverseQueue2(void(*pf)(T e)) {
+		int pos = front;
+		for (int s = 0; s < size; s++) {
+			(*pf)(entry[pos]);
+			pos = (pos + 1) % MaxQueue;
+		}
+	}
+
+
+};
+
+
+template<class T>
+void add2(T *e) {
+	*e += 2;
+}
+
+template<class T>
+void display(T e) {
+	cout<<e<<" ";
+}
+
+int main() {
+	//test
+	Queue<float>q1;
+	cout<<"is queue empty? "<< q1.queueEmpty()<<"\n";
+	cout<<"the size of queue: "<< q1.queueSize()<<"\n";
+	if (!q1.queueFull())q1.append(5);
+	if (!q1.queueFull())q1.append(10);
+	if (!q1.queueFull())q1.append(15);
+	if (!q1.queueFull())q1.append(20);
+	if (!q1.queueFull())q1.append(25);
+	cout<<"is queue empty? "<< q1.queueEmpty()<<"\n";
+	cout<<"the size of stack: "<< q1.queueSize()<<"\n";
+	q1.traverseQueue2(&display<float>);
+	printf("\n");
+	float x = 3;
+	if (!q1.queueEmpty())q1.serve(&x);
+	cout<<"x value after changing: "<< x<<"\n";
+	cout<<"the size of queue: "<< q1.queueSize()<<"\n";
+	q1.queuefirst(&x);
+	cout<<"x value after changing: "<< x<<"\n";
+	q1.traverseQueue(&add2<float>);
+	q1.traverseQueue2(&display<float>);
+	printf("\n");
+	return 0;
+}
